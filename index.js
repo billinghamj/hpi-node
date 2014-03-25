252ba329fffd64210d97dac2baaf0865e8e21073
Richard Marr <richard.marr@gmail.com>
2014-03-25 12:33:12 +0000

    Fixed a parsing bug, expecting an incorrect element at root level

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ exports.performHpiCheck = function( reg, config, callback ){
 		
 		var status = res.statusCode;
 		if ( status >= 200 && status < 300 ){
-			exports.parse( body, callback );
+			exports.parse( body, config, callback );
 		} else {
 			console.log('error: '+ status)
 			console.log(body)
@@ -48,7 +48,7 @@ exports.performHpiCheck = function( reg, config, callback ){
 
 }
 
-exports.parse = function( raw, callback ){
+exports.parse = function( raw, config, callback ){
 	
 	var parser = new require('xml2js').Parser();
 	parser.parseString( raw, function (err, result) {
@@ -59,42 +59,49 @@ exports.parse = function( raw, callback ){
 			
 			if ( config.debug ) config.debug( result );
 			
-			result =  result['soapenv:Envelope']['soapenv:Body'][0]['ns1:EnquiryResponse'][0]['ns1:RequestResults'][0]['ns1:Asset'][0]['ns1:PrimaryAssetData'][0];
-		
-			var cleaned = {
-				make : result['ns1:DVLA'][0]['ns1:Make'][0]['ns1:Description'][0],
-				model : result['ns1:DVLA'][0]['ns1:Model'][0]['ns1:Description'][0],
-				body : result['ns1:DVLA'][0]['ns1:Body'][0]['ns1:Description'][0],
-				colour : result['ns1:DVLA'][0]['ns1:Body'][0]['ns1:Colour'][0]['ns1:Current'][0]['ns1:Description'][0], // also previous colours
-				wheelplan : result['ns1:DVLA'][0]['ns1:Body'][0]['ns1:WheelPlan'][0]['ns1:Description'][0],
-				wheelplan_code : result['ns1:DVLA'][0]['ns1:Body'][0]['ns1:WheelPlan'][0]['ns1:Code'][0],
-				weight : result['ns1:DVLA'][0]['ns1:Body'][0]['ns1:Weight'][0],
-				engine_size : result['ns1:DVLA'][0]['ns1:Engine'][0]['ns1:Size'][0],
-				engine_no : result['ns1:DVLA'][0]['ns1:Engine'][0]['ns1:Number'][0],
-				fuel : result['ns1:DVLA'][0]['ns1:Engine'][0]['ns1:Fuel'][0]['ns1:Description'][0],
-				year : result['ns1:KeyDates'][0]['ns1:Manufactured'][0]['ns1:Year'][0],
-				doors = result['ns1:SMMT'][0]['ns1:Body'][0]['ns1:Description'][0],
-				transmission = result['ns1:SMMT'][0]['ns1:Transmission'][0]['ns1:Description'][0],
-				//keepers_changed : result['ns1:DVLA'][0]['ns1:Keepers'][0]['ns1:LastChangeOfKeeperDate'][0],
-				//keepers : result['ns1:DVLA'][0]['ns1:Keepers'][0],
-				//keydates : result['ns1:DVLA'][0]['ns1:KeyDates'][0],
-				isImported : result['ns1:KeyDates'][0]['ns1:IsImported'][0],
-				northernIreland : result['ns1:DVLA'][0]['ns1:IsFromNorthernIreland'][0],
-				/*checks : {
-					plate_transfers : result['ns1:FullCheck'][0]['ns1:PlateTransfers'][0]['$']['xsi:nil'] == "1",
-					security_watch : result['ns1:FullCheck'][0]['ns1:SecurityWatch'][0]['$']['xsi:nil'] == "1",
-					finance_agreements : result['ns1:FullCheck'][0]['ns1:FinanceAgreements'][0]['$']['xsi:nil'] == "1",
-					vcar : result['ns1:FullCheck'][0]['ns1:VCAR'][0]['$']['xsi:nil'] == "1",
-					stolen_incidents : result['ns1:FullCheck'][0]['ns1:StolenIncidents'][0]['$']['xsi:nil'] == "1"	
-				}*/
-				abi : result['ns1:TranslatePlus'][0]['ns1:Instep'][0]['ns1:Code'][0],
-				group50 : result['ns1:TranslatePlus'][0]['ns1:InsuranceGroup'][0],
-				groupSuffix : result['ns1:TranslatePlus'][0]['ns1:InsuranceGroupSuffix'][0],
-				securityCode : result['ns1:TranslatePlus'][0]['ns1:SecurityCode'][0],
-			};
+			try{
+				
+				result =  result['soapenv:Envelope']['soapenv:Body'][0]['ns1:EnquiryResponse'][0]['ns1:RequestResults'][0]['ns1:Asset'][0]['ns1:PrimaryAssetData'][0];
 			
-			// Default case - Assume one car at a time
-			return callback( undefined, cleaned ); 
+				var cleaned = {
+					make : get(result,['DVLA','Make','Description']),
+					model : get(result,['DVLA','Model','Description']),
+					body : get(result,['DVLA','Body','Description']),
+					colour : get(result,['DVLA','Body','Colour','Current','Description']), // also previous colours
+					wheelplan : get(result,['DVLA','Body','WheelPlan','Description']),
+					wheelplan_code : get(result,['DVLA','Body','WheelPlan','Code']),
+					weight : get(result,['DVLA','Body','Weight']),
+					engine_size : get(result,['DVLA','Engine','Size']),
+					engine_no : get(result,['DVLA','Engine','Number']),
+					fuel : get(result,['DVLA','Engine','Fuel','Description']),
+					year : get(result,['DVLA','KeyDates','Manufactured','Year']),
+					doors : get(result,['SMMT','Body','Description']),
+					transmission : get(result,['SMMT','Transmission','Description']),
+					//keepers_changed : result['ns1:DVLA'][0]['ns1:Keepers'][0]['ns1:LastChangeOfKeeperDate'][0],
+					//keepers : result['ns1:DVLA'][0]['ns1:Keepers'][0],
+					//keydates : result['ns1:DVLA'][0]['ns1:KeyDates'][0],
+					isImported : get(result,['DVLA','KeyDates','IsImported']),
+					northernIreland : get(result,['DVLA','IsFromNorthernIreland']),
+					/*checks : {
+						plate_transfers : result['ns1:FullCheck'][0]['ns1:PlateTransfers'][0]['$']['xsi:nil'] == "1",
+						security_watch : result['ns1:FullCheck'][0]['ns1:SecurityWatch'][0]['$']['xsi:nil'] == "1",
+						finance_agreements : result['ns1:FullCheck'][0]['ns1:FinanceAgreements'][0]['$']['xsi:nil'] == "1",
+						vcar : result['ns1:FullCheck'][0]['ns1:VCAR'][0]['$']['xsi:nil'] == "1",
+						stolen_incidents : result['ns1:FullCheck'][0]['ns1:StolenIncidents'][0]['$']['xsi:nil'] == "1"	
+					}*/
+					abi : get(result,['TranslatePlus','Instep','Code']),
+					group50 : get(result,['TranslatePlus','InsuranceGroup']),
+					groupSuffix : get(result,['TranslatePlus','InsuranceGroupSuffix']),
+					securityCode : get(result,['TranslatePlus','SecurityCode']),
+				};
+			
+				// Default case - Assume one car at a time
+				return callback( undefined, cleaned ); 
+			} catch (err) {
+				console.error(err.stack);
+				console.error( JSON.stringify( get(result,['DVLA','KeyDates']), null, '\t' ));
+				return callback(err);
+			}
 		
 		} catch (err){
 			return callback(err);
@@ -103,6 +110,15 @@ exports.parse = function( raw, callback ){
 	
 }
 
-
+// Helper to make it easy to traverse the object returned by xml2js
+// e.g. result['ns1:DVLA'][0]['ns1:Body'][0]['ns1:Colour'][0]['ns1:Current'][0]['ns1:Description'][0]
+function get( obj, propChain, ns ){
+	var ref = obj,
+		ns = ns ? ns+":" : "ns1:";
+	for ( var i in propChain ){
+		ref = ref[ns+propChain[i]][0];
+	}
+	return ref;
+}
 
 
