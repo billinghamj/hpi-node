b5de5e5a411ec7dcbd811c792cfea531ab1eb7c7
James Billingham <james@billingham.net>
2015-10-08 11:38:27 +0100

    Converted from callbacks to promises

--- index.js
+++ index.js
@@ -17,29 +17,38 @@ function get(obj, fields) {
 	return obj;
 }
 
-module.exports = function (reg, config, callback) {
+module.exports = function (reg, config) {
 	const query = serialise(reg, config);
 
-	const request = Request({
-		method: 'POST',
-		uri: config.url,
-		body: query,
-	}, function (error, res, body) {
-		if (error)
-			return callback(error);
+	return new Promise(function (resolve, reject) {
+		Request({
+			method: 'POST',
+			uri: config.url,
+			body: query,
+		}, function (error, response, body) {
+			if (error) {
+				reject(error);
+				return;
+			}
 
-		handleResponse(res.statusCode, body, config, callback);
-	});
+			response.body = body;
+			resolve(response);
+		});
+	})
+	.then(handleResponse);
 };
 
-function handleResponse(status, body, config, callback) {
-	if (status >= 200 && status < 300) {
-		parse(body, config, callback);
-	} else if (body && body.indexOf('VRM is invalid') > -1) {
-		return callback(new Error('VRM is invalid'));
-	} else {
-		return callback(new Error('HPI returned status code ' + status, body));
-	}
+function handleResponse(response) {
+	const status = response.statusCode;
+	const body = response.body;
+
+	if (status >= 200 && status < 300)
+		return parse(body);
+
+	if (body && body.indexOf('VRM is invalid') !== -1)
+		throw new Error('VRM is invalid');
+
+	throw new Error('HPI returned status code ' + status, body);
 }
 
 function serialise(reg, config) {
@@ -67,47 +76,46 @@ function serialise(reg, config) {
 		'</soap:Envelope>'].join('');
 }
 
-function parse(raw, config, callback) {
-	const parser = new XmlParser();
-
-	parser.parseString(raw, function (error, res) {
-		if (error)
-			return callback(error);
-
-		if (!res)
-			return callback(new Error('Failed to parse XML, not sure why this might happen'));
+function parse(raw) {
+	return new Promise(function (resolve, reject) {
+		const parser = new XmlParser();
+		parser.parseString(raw, function (error, result) {
+			if (error) reject(error);
+			else resolve(result);
+		});
+	})
+	.then(function (result) {
+		if (!result)
+			throw new Error('Failed to parse XML, not sure why this might happen');
 
 		const clean = {};
-		xmlCleanup(res['soapenv:Envelope']['soapenv:Body'][0], clean);
+		xmlCleanup(result['soapenv:Envelope']['soapenv:Body'][0], clean);
 
 		if (clean.Fault) {
 			const desc = get(clean.Fault, 'detail.HpiSoapFault.Error.Description');
 
 			if (!desc)
-				return callback(new Error('Unknown HPI error: ' + JSON.stringify(clean.Fault)));
+				throw new Error('Unknown HPI error: ' + JSON.stringify(clean.Fault));
 
-			if (desc === 'VRM is invalid')
-				return callback(new Error(desc));
-
-			return callback(new Error(desc));
+			throw new Error(desc);
 		}
 
 		const results = get(clean, 'EnquiryResponse.RequestResults');
 
 		if (!results)
-			return callback(new Error('Missing data: ' + JSON.stringify(clean)));
+			throw new Error('Missing data: ' + JSON.stringify(clean));
 
 		const data = get(results, 'Asset.PrimaryAssetData');
 		const warning = get(results, 'Warning');
 		const dvla = get(data, 'DVLA');
 
 		if (warning && !dvla)
-			return callback(new Error(warning.Description));
+			throw new Error(warning.Description);
 
 		if (!dvla)
-			return callback(new Error('Missing DVLA data: ' + JSON.stringify(data)));
+			throw new Error('Missing DVLA data: ' + JSON.stringify(data));
 
-		return callback(null, data);
+		return data;
   });
 }
 
